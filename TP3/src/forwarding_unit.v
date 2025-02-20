75891f14a4ece3e0311601cfcde38b98004114a3
Franco Rodríguez <122646722+rodriguezzfran@users.noreply.github.com>
2025-02-20 17:52:19 -0300

    Tp4/forwarding unit (#12)
    * forwarding unit totally functional (I hope so)
    
    * .
    
    * variables renamed
    
    ---------
    
    Co-authored-by: Bruno Guglielmotti <brunoguglielmotti@gmail.com>

--- TP3/src/forwarding_unit.v
+++ TP3/src/forwarding_unit.v
@@ -1,10 +1,10 @@
 module forwarding_unit (
-    input wire [4:0] i_ex_mem_rd, // proviene de la etapa de memoria
-    input wire [4:0] i_mem_wb_rd, // proviene de la etapa de write back
-    input wire [4:0] i_id_ex_rs, // numero rs de la etapa de ejecución
-    input wire [4:0] i_id_ex_rt, // numero rt de la etapa de ejecución
-    input wire i_ex_mem_regwrite, // señal de escritura en el registro de la etapa de memoria
-    input wire i_mem_wb_regwrite, // señal de escritura en el registro de la etapa de write back
+    input wire [4:0] i_rd_MEM, // proviene de la etapa de memoria
+    input wire [4:0] i_rd_WB, // proviene de la etapa de write back
+    input wire [4:0] i_rs_EX, // numero rs de la etapa de ejecución
+    input wire [4:0] i_rt_EX, // numero rt de la etapa de ejecución
+    input wire i_regwrite_MEM, // señal de escritura en el registro de la etapa de memoria
+    input wire i_regwrite_WB, // señal de escritura en el registro de la etapa de write back
     output reg [1:0] o_forward_a, // señal de forwarding para la entrada A
     output reg [1:0] o_forward_b // señal de forwarding para la entrada B
 );
@@ -18,32 +18,29 @@ end
 // forwarding para A
 always @(*) begin
 
-    if( i_ex_mem_regwrite && (i_ex_mem_rd != 5'b00000) && (i_ex_mem_rd == i_id_ex_rs) ) begin
+    if( i_regwrite_MEM && (i_rd_MEM != 5'b00000) && (i_rd_MEM == i_rs_EX) ) begin
         o_forward_a = 2'b10; // forward desde la etapa de memoria
     end
-    else if( i_mem_wb_regwrite && (i_mem_wb_rd != 5'b00000) && (i_mem_wb_rd == i_id_ex_rs) ) begin
+    else if( i_regwrite_WB && (i_rd_WB != 5'b00000) && (i_rd_WB == i_rs_EX) ) begin
         o_forward_a = 2'b01; // forward desde la etapa de write back
     end
     else begin
         o_forward_a = 2'b00; // no hay forwarding
     end
-
 end
 
 // forwarding para B
 always @(*) begin
 
-    if( i_ex_mem_regwrite && (i_ex_mem_rd != 5'b00000) && (i_ex_mem_rd == i_id_ex_rt) ) begin
+    if( i_regwrite_MEM && (i_rd_MEM != 5'b00000) && (i_rd_MEM == i_rt_EX) ) begin
         o_forward_b = 2'b10; // forward desde la etapa de memoria
     end
-    else if( i_mem_wb_regwrite && (i_mem_wb_rd != 5'b00000) && (i_mem_wb_rd == i_id_ex_rt) ) begin
+    else if( i_regwrite_WB && (i_rd_WB != 5'b00000) && (i_rd_WB == i_rt_EX) ) begin
         o_forward_b = 2'b01; // forward desde la etapa de write back
     end
     else begin
         o_forward_b = 2'b00; // no hay forwarding
     end
 end
-
-
 
 endmodule
