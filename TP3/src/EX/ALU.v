41f6507e53cf823655082298438febb8b4eed8cb
Bruno Guglielmotti <129474500+BrunoGugli@users.noreply.github.com>
2025-02-19 21:09:57 -0300

    Execution stage (#16)
    * inputs and outputs
    
    * stage and ALU finished
    
    * alu instantiated
    
    * missing localparams fix
    
    * endline fix
    
    * fix control signals as output
    
    * Fix in LUI operation at the ALU
    
    * tb execution done
    
    ---------
    
    Co-authored-by: Rodriguezzfran <Francorod47@gmail.com>

--- TP3/src/EX/ALU.v
+++ TP3/src/EX/ALU.v
@@ -1,24 +1,69 @@
 module ALU #(parameter NB_OP = 6,
-             parameter NB_DATA = 8,
-             parameter NB_OUT = 16
+             parameter NB_DATA = 32
 )(
     input wire signed [NB_DATA-1:0] i_operand1,  // Primer operando de 8 bits
     input wire signed [NB_DATA-1:0] i_operand2,  // Segundo operando de 8 bits
     input wire [NB_OP-1:0] i_opcode,            // Código de operación de 6 bits
-    output reg signed [NB_OUT-1:0] o_result    // Resultado de la operación
+    input wire [4:0] i_shamt,                    // Cantidad de desplazamiento
+    output reg [NB_DATA-1:0] o_result    // Resultado de la operación
 );
+
+localparam IDLE_OPCODE  = 6'b111111;
+localparam SLL_OPCODE   = 6'b000000;
+localparam SRL_OPCODE   = 6'b000010;
+localparam SRA_OPCODE   = 6'b000011;
+localparam SLLV_OPCODE  = 6'b000100;
+localparam SRLV_OPCODE  = 6'b000110;
+localparam SRAV_OPCODE  = 6'b000111;
+localparam ADD_OPCODE   = 6'b100000;
+localparam ADDU_OPCODE  = 6'b100001;
+localparam SUB_OPCODE   = 6'b100010;
+localparam SUBU_OPCODE  = 6'b100011;
+localparam AND_OPCODE   = 6'b100100;
+localparam OR_OPCODE    = 6'b100101;
+localparam XOR_OPCODE   = 6'b100110;
+localparam NOR_OPCODE   = 6'b100111;
+localparam SLT_OPCODE   = 6'b101010;
+localparam SLTU_OPCODE  = 6'b101011;
+
+localparam ADDI_OPCODE  = 6'b001000;
+localparam ADDIU_OPCODE = 6'b001001;
+localparam SLTI_OPCODE  = 6'b001010;
+localparam SLTIU_OPCODE = 6'b001011;
+localparam ANDI_OPCODE  = 6'b001100;
+localparam ORI_OPCODE   = 6'b001101;
+localparam XORI_OPCODE  = 6'b001110;
+localparam LUI_OPCODE   = 6'b001111;
     
 always @(*) begin
     case (i_opcode)
-        6'b100000: o_result = i_operand1 + i_operand2; // ADD
-        6'b100010: o_result = i_operand1 - i_operand2; // SUB
-        6'b100100: o_result = i_operand1 & i_operand2; // AND
-        6'b100101: o_result = i_operand1 | i_operand2; // OR
-        6'b100110: o_result = i_operand1 ^ i_operand2; // XOR
-        6'b000011: o_result = i_operand1 >>> i_operand2; // SRA
-        6'b000010: o_result = i_operand1 >>  i_operand2; // SRL
-        6'b100111: o_result = ~(i_operand1 | i_operand2); // NOR
-        default: o_result = 8'b00000000; // Default
+        ADD_OPCODE:o_result = i_operand1 + i_operand2;
+        SUB_OPCODE:o_result = i_operand1 - i_operand2;
+        IDLE_OPCODE:o_result = 0;
+        SLL_OPCODE:o_result = i_operand1 << i_shamt;
+        SRL_OPCODE:o_result = i_operand1 >> i_shamt;
+        SRA_OPCODE:o_result = i_operand1 >>> i_shamt;
+        SLLV_OPCODE:o_result = i_operand1 << i_operand2;
+        SRLV_OPCODE:o_result = i_operand1 >> i_operand2;
+        SRAV_OPCODE:o_result = i_operand1 >>> i_operand2;
+        ADDU_OPCODE:o_result = $unsigned(i_operand1) + $unsigned(i_operand2);
+        SUBU_OPCODE:o_result = $unsigned(i_operand1) - $unsigned(i_operand2);
+        AND_OPCODE:o_result = i_operand1 & i_operand2;
+        OR_OPCODE:o_result = i_operand1 | i_operand2;
+        XOR_OPCODE:o_result = i_operand1 ^ i_operand2;
+        NOR_OPCODE:o_result = ~(i_operand1 | i_operand2);
+        SLT_OPCODE:o_result = (i_operand1 < i_operand2) ? 1 : 0;
+        SLTU_OPCODE:o_result = ($unsigned(i_operand1) < $unsigned(i_operand2)) ? 1 : 0;
+
+        ADDI_OPCODE:o_result = i_operand1 + i_operand2;
+        ADDIU_OPCODE:o_result = $unsigned(i_operand1) + $unsigned(i_operand2);
+        SLTI_OPCODE:o_result = (i_operand1 < i_operand2) ? 1 : 0;
+        SLTIU_OPCODE:o_result = ($unsigned(i_operand1) < $unsigned(i_operand2)) ? 1 : 0;
+        ANDI_OPCODE:o_result = i_operand1 & i_operand2;
+        ORI_OPCODE:o_result = i_operand1 | i_operand2;
+        XORI_OPCODE: o_result = i_operand1 ^ i_operand2;
+        LUI_OPCODE: o_result = {i_operand2[15:0], 16'b0};
+        default: o_result = 0;
     endcase
 end
 
---
+++ TP3/src/EX/intruction_exec.v
@@ -0,0 +1,192 @@
+module instruction_exec (
+    input wire i_clk,
+    input wire i_reset,
+    input wire i_halt,
+
+    // señales de control
+    input wire i_ctl_EX_reg_dest_EX,
+    input wire i_ctl_EX_alu_src_EX,
+    input wire [1:0] i_ctl_EX_alu_op_EX,
+
+    input wire i_ctl_MEM_mem_read_EX,
+    input wire i_ctl_MEM_mem_write_EX,
+    input wire i_ctl_MEM_unsigned_EX,
+    input wire [1:0] i_ctl_MEM_data_width_EX,
+
+    input wire i_ctl_WB_mem_to_reg_EX,
+    input wire i_ctl_WB_reg_write_EX,
+
+    // lo que viene del ID
+    input wire [31:0] i_RA,
+    input wire [31:0] i_RB,
+    input wire [4:0] i_rs,
+    input wire [4:0] i_rt,
+    input wire [4:0] i_rd,
+    input wire [5:0] i_funct,
+    input wire [31:0] i_inmediate,
+    input wire [5:0] i_opcode,
+    input wire [4:0] i_shamt,
+
+    // de la forward unit
+    input wire [1:0] i_forward_A,
+    input wire [1:0] i_forward_B,
+
+    // datos de mem y write-back
+    input wire [31:0] i_MEM_ALU_result,
+    input wire [31:0] i_WB_read_data,
+
+    // señales de control output
+    output reg o_ctl_MEM_mem_read_EX,
+    output reg o_ctl_MEM_mem_write_EX,
+    output reg o_ctl_MEM_unsigned_EX,
+    output reg [1:0] o_ctl_MEM_data_width_EX,
+    output reg o_ctl_WB_mem_to_reg_EX,
+    output reg o_ctl_WB_reg_write_EX,
+
+    // lo que va a MEM
+    output reg [31:0] o_ALU_result,
+    output reg [31:0] o_data_to_write,
+    output reg [4:0] o_reg_dest
+);
+
+    reg signed [31:0] operand_A;
+    reg signed [31:0] fw_operand_B;
+    reg signed [31:0] operand_B;
+    wire [31:0] ALU_result_wire;
+    reg [5:0] alu_opcode;
+
+    localparam ADD_OPCODE = 6'b100000;
+    localparam IDLE_OPCODE = 6'b111111;
+    localparam JAL_OPCODE = 6'b000011;
+    localparam R_TYPE_OPCODE = 6'b000000;
+    localparam JALR_FUNCT = 6'b001001;
+
+        // ALU
+    ALU #(
+        .NB_OP(6),
+        .NB_DATA(32)
+    ) alu_inst (
+        .i_operand1(operand_A),
+        .i_operand2(operand_B),
+        .i_opcode(alu_opcode),
+        .o_result(ALU_result_wire),
+        .i_shamt(i_shamt)
+    );
+
+    // alu control
+    always @(*) begin
+        case(i_ctl_EX_alu_op_EX)
+            2'b00: begin // Loads, stores, jalr, jal
+                alu_opcode = ADD_OPCODE;
+            end
+
+            2'b01: begin // Branch, no hace nada porque no se necesita ALU
+                alu_opcode = IDLE_OPCODE;
+            end
+
+            2'b10: begin // Tipo R, se toma el campo funct
+                alu_opcode = i_funct;
+            end
+
+            2'b11: begin // Tipo I con inmediato, se toma el opcode
+                alu_opcode = i_opcode;
+            end
+            default: begin
+                alu_opcode = IDLE_OPCODE;
+            end
+        endcase
+    end
+
+    // forwarding A
+    always @(*) begin
+        case(i_forward_A)
+            2'b00: begin
+                operand_A = i_RA;
+            end
+            2'b01: begin
+                operand_A = i_WB_read_data;
+            end
+            2'b10: begin
+                operand_A = i_MEM_ALU_result;
+            end
+            default: begin
+                operand_A = 0;
+            end
+        endcase
+
+        if(i_opcode == JAL_OPCODE || (i_opcode == R_TYPE_OPCODE && i_funct == JALR_FUNCT)) begin
+            operand_A = i_RA;
+        end
+    end
+
+    // forwarding B
+    always @(*) begin
+        case(i_forward_B)
+            2'b00: begin
+                fw_operand_B = i_RB;
+            end
+            2'b01: begin
+                fw_operand_B = i_WB_read_data;
+            end
+            2'b10: begin
+                fw_operand_B = i_MEM_ALU_result;
+            end
+            default: begin
+                fw_operand_B = 0;
+            end
+        endcase
+
+        if(i_opcode == JAL_OPCODE || (i_opcode == R_TYPE_OPCODE && i_funct == JALR_FUNCT)) begin
+            fw_operand_B = i_RB;
+        end
+    end
+
+    // alu src control
+    always @(*) begin
+        if(i_ctl_EX_alu_src_EX) begin
+            operand_B = i_inmediate;
+        end else begin
+            operand_B = fw_operand_B;
+        end
+    end
+
+    // salidas a MEM
+    always @(posedge i_clk) begin
+        if(i_reset) begin
+            o_ALU_result <= 32'h00000000;
+            o_data_to_write <= 32'h00000000;
+            o_reg_dest <= 5'b00000;
+        end else if(~i_halt) begin
+            // reg dest
+            o_reg_dest <= i_ctl_EX_reg_dest_EX ? i_rd : i_rt;
+
+            // Alu result
+            o_ALU_result <= ALU_result_wire;
+
+            // data to write in mem
+            o_data_to_write <= fw_operand_B; // aca capaz tire error por el signed
+        end
+    end
+
+    // señales de control
+    always @(posedge i_clk) begin
+        if(i_reset) begin
+            o_ctl_MEM_mem_read_EX <= 1'b0;
+            o_ctl_MEM_mem_write_EX <= 1'b0;
+            o_ctl_MEM_unsigned_EX <= 1'b0;
+            o_ctl_MEM_data_width_EX <= 2'b00;
+            o_ctl_WB_mem_to_reg_EX <= 1'b0;
+            o_ctl_WB_reg_write_EX <= 1'b0;
+        end else begin
+            if(~i_halt) begin
+                o_ctl_MEM_mem_read_EX       <= i_ctl_MEM_mem_read_EX;
+                o_ctl_MEM_mem_write_EX      <= i_ctl_MEM_mem_write_EX;
+                o_ctl_MEM_unsigned_EX       <= i_ctl_MEM_unsigned_EX;
+                o_ctl_MEM_data_width_EX     <= i_ctl_MEM_data_width_EX;
+                o_ctl_WB_mem_to_reg_EX      <= i_ctl_WB_mem_to_reg_EX;
+                o_ctl_WB_reg_write_EX       <= i_ctl_WB_reg_write_EX;
+            end
+        end
+    end
+
+endmodule
