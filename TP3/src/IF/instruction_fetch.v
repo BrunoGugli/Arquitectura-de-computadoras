bc39b56a7a2db52aa5bd45c9e066f9cc036b21e1
Bruno Guglielmotti <brunoguglielmotti@gmail.com>
2025-03-29 18:38:33 -0300

    memory implemented as ram

--- TP3/src/IF/instruction_fetch.v
+++ TP3/src/IF/instruction_fetch.v
@@ -37,7 +37,7 @@ xilinx_one_port_ram_async #(
 ) instruccion_mem (
     .i_clk(i_clk),
     .i_we(i_write_instruction_flag),
-    .i_writing_data_width(2'b11), // 32 bits
+    //.i_writing_data_width(2'b11), // 32 bits
     .i_addr(address_instruction[INST_MEM_ADDR_WIDTH-1:0]), 
     .i_data(i_instruction_to_write),
     .o_data(instruction_from_memory)
