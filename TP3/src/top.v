d2800f60ddb5ff4be36004642d0749d6afdce3ad
Rodriguezzfran <francorod47@gmail.com>
2025-04-01 15:20:39 -0300

    changed 100mhz to 50Mhz

--- TP3/src/top.v
+++ TP3/src/top.v
@@ -71,11 +71,11 @@ module top_pipeline#(
 
     // Instancia del generador de tasa de baudios
     baud_rate_gen #(
-        .clk_freq(100000000),  // Reloj de 100 MHz
+        .clk_freq(50000000),  // Reloj de 100 MHz
         .baud_rate(9600)      // Tasa de baudios de 9600 bps
     )
     u_baud_rate_gen (
-        .i_clk(i_clk),        // Conectar al reloj del sistema
+        .i_clk(clk_wzrd_out1),        // Conectar al reloj del sistema
         .i_reset(sys_reset),    // Conectar a la señal de reset
         .i_valid(1'b1),       // Siempre habilitar la generación baud_rate
         .o_baud_tick(baud_tick)  // baud_tick de salida para el receptor
@@ -87,7 +87,7 @@ module top_pipeline#(
         .STP_BITS_TICKS(STP_BITS_TICKS)   // 16 ticks para el bit de parada
     )
     u_uart_receiver (
-        .i_clk(i_clk),        // Conectar al reloj del sistema
+        .i_clk(clk_wzrd_out1),        // Conectar al reloj del sistema
         .i_reset(sys_reset),    // Conectar a la señal de reset
         .i_rx(i_rx),          // Conectar a la línea serial de entrada
         .i_bd_tick(baud_tick),// Conectar el baud tick desde baud_rate_gen
@@ -101,7 +101,7 @@ module top_pipeline#(
         .FIFO_ADDR_WIDTH(8)  // 75 elementos de profundidad
     )
     u_fifo_transmitter (
-        .i_clk(i_clk),        // Conectar al reloj del sistema
+        .i_clk(clk_wzrd_out1),        // Conectar al reloj del sistema
         .i_reset(sys_reset),    // Conectar a la señal de reset
         .i_wr(top_fifo_write_en),       // No habilitar la escritura
         .i_rd(top_read_new_data),       // Habilitar la lectura
@@ -117,7 +117,7 @@ module top_pipeline#(
         .STP_BITS_TICKS(STP_BITS_TICKS)     // 16 ticks for stop bit
     )
     u_uart_transmitter (
-        .i_clk(i_clk),          // Connect to system clock
+        .i_clk(clk_wzrd_out1),          // Connect to system clock
         .i_reset(sys_reset),      // Connect to reset signal
         .i_tx_start(~top_transmit), 
         .i_bd_tick(baud_tick),  // Connect baud tick from baud_rate_gen
